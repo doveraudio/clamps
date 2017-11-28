2c3c4582646dcb736e9817ffafafd361135117b2
Jeremy Leach <doveraudio@gmail.com>
2017-11-28 13:00:51 -0500

    couple fixes to provisioning script

--- data/getpassword.sh
+++ data/getpassword.sh
@@ -4,14 +4,14 @@
 
 printf "Setting up MySQL for developer mode.\n";
 
-mysqlpass="$(grep 'A temporary password is generated for root@localhost' /var/log/mysqld.log |tail -1 | awk '{print $NF }')\n";
-echo " Logging into MYSQL With Password: ${mysqlpass}\n";
+mysqlpass="$(grep 'A temporary password is generated for root@localhost' /var/log/mysqld.log |tail -1 | awk '{print $NF }')";
+echo " Logging into MYSQL With Password: ${mysqlpass}";
 echo "Removing \'validate_password\' plugin, setting up root for dev...\n";
 echo ".\n.\n.\n";
 
 
-mysqlpass="complicatedPassword_1";
-mysql -u root -p${mysqlpass} --connect-expired-password < initmysql.sql;
+#mysqlpass="complicatedPassword_1";
+mysql -u root -p${mysqlpass} --connect-expired-password < /var/data/initmysql.sql;
 
 printf "MySQL User:root , Password:root\n";
 printf "Configuration Complete.\n";
--- data/provision.sh
+++ data/provision.sh
@@ -25,14 +25,19 @@ printf "Copying new php.ini file\n";
 sudo cp /var/data/php.ini /etc/php.ini;
 printf "Restarting apache...\n";
 sudo apachectl restart;
+#grep 'A temporary password is generated for root@localhost' /var/log/mysqld.log |tail -1 | awk '{print $NF }' >> /var/data/mysqlpass.txt
+
 
 printf "Setting up MySQL for developer mode.\n";
 
-mysqlpass="$(grep 'A temporary password is generated for root@localhost' /var/log/mysqld.log |tail -1 | awk '{print $NF }')\n";
-echo " Logging into MYSQL With Password: ${mysqlpass}\n";
-echo "Removing `validate_password` plugin, setting up root for dev...\n\n";
+mysqlpass="$(grep 'A temporary password is generated for root@localhost' /var/log/mysqld.log |tail -1 | awk '{print $NF }')";
+echo " Logging into MYSQL With Password: ${mysqlpass}";
+echo "Removing \'validate_password\' plugin, setting up root for dev...\n";
 echo ".\n.\n.\n";
-mysql -u root -p${mysqlpass} --connect-expired-password < initmysql.sql;
+
+
+#mysqlpass="complicatedPassword_1";
+mysql -u root -p${mysqlpass} --connect-expired-password < /var/data/initmysql.sql;
 
 printf "MySQL User:root , Password:root\n";
 printf "Configuration Complete.\n";
